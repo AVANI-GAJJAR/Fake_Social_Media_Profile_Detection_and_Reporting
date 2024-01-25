d71fb892db851d3cb754bc6164a41cd65b9464b0
AVANI-GAJJAR <78599804+AVANI-GAJJAR@users.noreply.github.com>
2024-01-25 15:52:29 +0530

    Update settings.py
    allowing all host

--- Fake_Profile_Detection/settings.py
+++ Fake_Profile_Detection/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7@7w7qhzn5iz)rn6)hmh@3l)3ck3u*11a=2glwtdyw*(rq4z2y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
